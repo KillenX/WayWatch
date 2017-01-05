0ae0df939f0d79be79b4a8dd26ac4cde69adffb2
Srdjan Hajder <igorisrdjan@gmail.com>
2017-01-05 03:37:45 +0100

    Add graph Supports: loading from files saving to files editing of the graph layout finding minimum journey time between two points brainless initialization

--- WW Entry/Source.cpp
+++ WW Entry/Source.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <ios>
 #include "../common/UserManager.h"
+#include "Graph.h"
+
+using std::cout;
+using std::ios;
+using std::cin;
+
 
 int main()
 {
+	Graph g(5);
+	g.bootUp();
+
 	
+
+	cin.ignore();
+	cin.get();
 	return 0;
 }
