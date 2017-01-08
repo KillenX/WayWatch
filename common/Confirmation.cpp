06b8a9b122b0b6660d751e94c95c4d01e4bfb63d
bajra <bajra@DESKTOP-MURED24>
2017-01-08 12:46:08 +0100

    Add readConfirmation Fix generateConfirmation

--- common/Confirmation.cpp
+++ common/Confirmation.cpp
@@ -13,10 +13,8 @@ void generateConirmation(unsigned counter, std::string enterence, std::string ca
 	std::ostringstream convert;
 	convert << counter;
 	number = convert.str();
-
+	//TODO: Check if the confirmation with the same serial number exists
 	std::ofstream confirmation("confirmation" + number + ".txt", std::ios::out); //new confirmation made
-
-	std::cout << std::put_time(&tm, "%d-%m-%Y %H:%M:%S") << std::endl;
 	
 	std::ostringstream date;
 	std::ostringstream time;
@@ -25,10 +23,42 @@ void generateConirmation(unsigned counter, std::string enterence, std::string ca
 	time << std::put_time(&tm, "%H:%M:%S");
 
 	confirmation << "========================================" << std::endl; //40 chars long
-	confirmation << "Enterence:       " << enterence << std::endl; //12 chars long
+	confirmation << "Enterence:       " << enterence << std::endl; //17 chars long
 	confirmation << "Date:            " << date.str() << std::endl;
 	confirmation << "Time:            " << time.str() << std::endl;
 	confirmation << "Category:        " << category << std::endl;
 	confirmation << "Register plates: " << regiterPlates << std::endl;
 	confirmation << "========================================" << std::endl;
+}
+
+std::tuple<std::string, std::string, std::string, std::string> readConfirmation(unsigned ID)
+{
+	std::stringstream convert;
+	convert << ID;
+
+	std::ifstream confirmation("confirmation" + convert.str() + ".txt");
+	if (!confirmation.is_open())
+		convert << "bla";//TODO:Throw exception
+	std::string enterence;
+	std::string registrationPlates;
+	std::string date;
+	std::string time;
+	std::string category;
+	//Code that is heavily dependant on the look of the confirmationX.txt file
+	confirmation.ignore(41, '\n');
+	confirmation.ignore(17);
+	std::getline(confirmation, enterence);
+	confirmation.ignore(17);
+	std::getline(confirmation, date);
+	confirmation.ignore(17);
+	std::getline(confirmation, time);
+	confirmation.ignore(17);
+	std::getline(confirmation, category);
+	confirmation.ignore(17);
+	std::getline(confirmation, registrationPlates);
+
+	confirmation.close();
+	
+	return std::make_tuple < std::string, std::string, std::string, std::string, std::string>
+		(enterence, date, time, category, registrationPlates);
 }
--- common/Confirmation.h
+++ common/Confirmation.h
@@ -1,3 +1,5 @@
 #pragma once
 #include <string>
+#include <tuple>
 void generateConirmation(unsigned, std::string, std::string, std::string);
+std::tuple<std::string, std::string, std::string, std::string, std::string> readConfirmation(unsigned);
