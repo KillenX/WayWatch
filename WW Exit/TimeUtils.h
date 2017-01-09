b9101beea2086b02803ebcebd1d881f0d0e0ecd7
Ciro <stefbl96@gmail.com>
2017-01-09 16:31:29 +0100

    fix: string2time

--- WW Exit/TimeUtils.h
+++ WW Exit/TimeUtils.h
@@ -1,10 +1,9 @@
 #pragma once
 #include <string>
 #include <ctime>
-#include <istream>
 
 namespace TimeUtils
 {
 	std::string Time2String(const std::time_t &time, const char *formatString);
-	std::time_t StringStream2Time(std::istream &is, const char *formatString);
+	std::time_t String2Time(std::string string, const char *formatString);
 }
